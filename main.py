9e4b1d2231b5671970240ed22822dbf8396d8ef1
msheridan1228 <53874087+msheridan1228@users.noreply.github.com>
2025-07-16 16:52:17 -0400

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from hierarchical_content_taxonomy.hierarchical_taxonomy import HierarchicalTaxonomy
-
-urls = ['https://example.com/wp-json/wp/v2/posts']
-taxonomy = HierarchicalTaxonomy(urls)
-taxonomy.pull_data()
-taxonomy.generate_cluster_plots()
-taxonomy.create_clusters([0.5, 1.0, 1.5])
-data = taxonomy.get_data()
-print(data.head())
